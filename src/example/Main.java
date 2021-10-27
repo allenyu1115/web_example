863f0bc8316761f978afb7f48448481d4b0775d0
Allen Yu <allenyu@AllendeMacBook-Pro.local>
2021-10-27 20:06:56 +0800

    modify main

--- src/example/Main.java
+++ src/example/Main.java
@@ -2,7 +2,7 @@ package example;
 
 public class Main {
 
-  public static void main( String[] args ) {
-
-  }
+	public static void main(String[] args) {
+		System.out.println("hello, world");
+	}
 }
