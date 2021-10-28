074873ef12e210e6f5e71f5c5ff82ba63ca9cef5
ayu <allen.yu@aspiraconnect.com>
2021-10-28 16:26:33 +0800

    py example

--- src/example/Main.java
+++ src/example/Main.java
@@ -1,8 +1,64 @@
 package example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+	public static class Holder {
+		private int i;
+
+		public Holder(int i) {
+			this.setI(i);
+		}
+
+		public int getI() {
+			return i;
+		}
+
+		public void setI(int i) {
+			this.i = i;
+		}
+	}
+	
+	public static int increase(int i) {
+		return i + 1;
+	}
+
+	public static int decrease(int i) {
+		return i - 1;
+	}
+
+	public static Runnable increase(Holder holder) {
+		holder.setI(increase(holder.getI()));
+		return () -> holder.setI(decrease(holder.getI()));
+	}
 
 	public static void main(String[] args) {
-		System.out.println("hello, world!");
+             Holder holder = new Holder(1);
+             System.out.println(holder.getI());
+             
+             List<Runnable> decreaseList = new ArrayList<>();
+             
+             decreaseList.add(increase(holder));
+             decreaseList.add(increase(holder));
+             decreaseList.add(increase(holder));
+             decreaseList.add(increase(holder));
+             
+             System.out.println(holder.getI());
+             
+             Runnable lastOne = decreaseList.remove(decreaseList.size() -1);
+             lastOne.run();
+             
+             System.out.println(holder.getI());
+             
+             Runnable secondOne = decreaseList.remove(decreaseList.size() -1);
+             Runnable thirdOne = decreaseList.remove(decreaseList.size() -1);
+             
+             secondOne.run();
+             thirdOne.run();
+             
+             System.out.println(holder.getI());
+             
+             
 	}
 }
