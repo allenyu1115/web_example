df5c6e2700d147e9981261d46bff91fe01bc1791
ayu <allen.yu@aspiraconnect.com>
2021-11-04 15:06:25 +0800

    test

--- src/example/Main.java
+++ src/example/Main.java
@@ -5,63 +5,6 @@ import java.util.List;
 
 public class Main {
 
-	public static enum CharType {
-		operator, number, undefine, blank, end
-	}
-
-	public static String getSExpression(String str) {
-		if (str.length() == 0) {
-			return str;
-		} else {
-			return getSExpressionRecursive(str, CharType.undefine, "", "", "");
-		}
-
-	}
-
-	private static String getSExpressionRecursive(String str, CharType lastCharType, String currentNum,
-			String lastNumberStr, String lastOperator) {
-		String currentChar = str.isEmpty() ? "" : str.substring(0, 1);
-		CharType currentCharType = getCharType(currentChar);
-		if (currentCharType.equals(CharType.number)) {
-			return getSExpressionRecursive(str.substring(1, str.length()), currentCharType, currentNum + currentChar,
-					lastNumberStr, lastOperator);
-		} else if (currentCharType.equals(CharType.operator)) {
-			return getSExpressionRecursive(str.substring(1, str.length()), currentCharType, "",
-					(lastOperator != "" && lastNumberStr != "")
-							? combineToSExpression(lastOperator, lastNumberStr, currentNum)
-							: currentNum,
-					currentChar);
-		} else if (currentCharType.equals(CharType.blank)) {
-			return getSExpressionRecursive(str.substring(1, str.length()), lastCharType, currentNum, lastNumberStr,
-					lastOperator);
-		} else if (currentCharType.equals(CharType.end)) {
-			return (lastNumberStr != "" && lastOperator != "")
-					? combineToSExpression(lastOperator, lastNumberStr, currentNum)
-					: "";
-		}
-		return "";
-
-	}
-
-	private static CharType getCharType(String currentChar) {
-		CharType currentCharType = CharType.undefine;
-		if (currentChar.equals(" ")) {
-			currentCharType = CharType.blank;
-		} else if (currentChar == null || currentChar.equals(""))
-			currentCharType = CharType.end;
-		else if (currentChar.chars().allMatch(Character::isDigit)) {
-			currentCharType = CharType.number;
-		} else if (currentChar.equals("+") || currentChar.equals("-") || currentChar.equals("*")
-				|| currentChar.equals("/")) {
-			currentCharType = CharType.operator;
-		}
-		return currentCharType;
-	}
-
-	private static String combineToSExpression(String operator, String leftNum, String rightNum) {
-		return "(" + operator + " " + leftNum + " " + rightNum + ")";
-	}
-
 	public static class Holder {
 		private int i;
 
@@ -122,6 +65,5 @@ public class Main {
 		thirdOne.run();
 
 		System.out.println(holder.getI());
-		System.out.println(getSExpression("71 + 8 * 76 - 899 / 5"));
 	}
 }
