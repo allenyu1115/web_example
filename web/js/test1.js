4994721b4624048195865c3fed9fd81f31288999
ayu <allen.yu@aspiraconnect.com>
2021-11-10 17:07:37 +0800

    test

---
+++ web/js/test1.js
@@ -0,0 +1,9 @@
+$().ready(
+	function() {
+		checkfunc = { true: function(x) { x.show(); }, false: function(x) { x.hide(); } }
+		$('#selectGrp > input[type="radio"] ').click(function(myevent) {
+			$('#bindGrp > div').each(function(_index, value) {
+				checkfunc[value.id == (myevent.target.id + 'Bind')]($('#' + value.id))
+			});
+		})
+	})
