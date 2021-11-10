3927e9528ad46a342dd3ba6a0fac579eb616eade
ayu <ayu@gmail.com>
2021-11-10 21:07:00 +0800

    test

--- web/js/test2.js
+++ web/js/test2.js
@@ -5,7 +5,7 @@ $().ready(function() {
 		let added_line = '<br>add new content'
 		$('#show').click(function() {
 			$('#mydefault').html(show_added ? init_line : init_line + added_line)
-			$('#show').html(show_added ? 'view all' : 'show less')
+			$('#show').html(show_added ? 'show all' : 'show less')
 			$('#mydefault').css(show_added ? { 'height': '1.5em', 'overflow': 'hidden' } : {
 				'height': '3em', 'overflow': 'auto'
 			});
