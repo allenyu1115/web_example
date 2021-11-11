3f7d525f7beb580f5c1bbc395c9b9b228245bd3d
ayu <allenyu>
2021-11-11 23:28:29 +0800

    test

--- web/js/test1.js
+++ web/js/test1.js
@@ -1,9 +1,12 @@
 $().ready(
-	function() {
+	function() {	
+		(function() {
 		checkfunc = { true: function(x) { x.show(); }, false: function(x) { x.hide(); } }
 		$('#selectGrp > input[type="radio"] ').click(function(myevent) {
 			$('#bindGrp > div').each(function(_index, value) {
 				checkfunc[value.id == (myevent.target.id + 'Bind')]($('#' + value.id))
 			});
 		})
-	})
+	    })()
+	}
+)
